35c68a61e9508d1a672e87b806a2d4a71cda96dc
Jamis Buck <jamis@37signals.com>
2008-03-19 16:43:47 -0600

    docs for Net::SSH::Config

--- lib/net/ssh/config.rb
+++ lib/net/ssh/config.rb
@@ -1,14 +1,95 @@
 module Net; module SSH
+
+  # The Net::SSH::Config class is used to parse OpenSSH configuration files,
+  # and translates that syntax into the configuration syntax that Net::SSH
+  # understands. This lets Net::SSH scripts read their configuration (to
+  # some extent) from OpenSSH configuration files (~/.ssh/config, /etc/ssh_config,
+  # and so forth).
+  #
+  # Only a subset of OpenSSH configuration options are understood:
+  #
+  # * Ciphers => maps to the :encryption option
+  # * ConnectTimeout => maps to the :timeout option
+  # * HostBasedAuthentication => maps to the :auth_methods option
+  # * HostKeyAlgorithms => maps to :host_key option
+  # * IdentityFile => maps to the :keys option
+  # * Macs => maps to the :hmac option
+  # * PasswordAuthentication => maps to the :auth_methods option
+  # * PreferredAuthentications => maps to the :auth_methods option
+  #
+  # Compression, CompressionLevel, ForwardAgent, Port, RekeyLimit are map to
+  # options of the same name (snake-cased, rather than camel-cased).
+  #
+  #--
+  # FIXME: GlobalKnownHostsFile, HostKeyAlias, HostName, LocalForward, SendEnv, User, UserKnownHostsFile
+  #++
+  #
+  # Note that you will never need to use this class directly--you can control
+  # whether the OpenSSH configuration files are read by passing the :config
+  # option to Net::SSH.start. (They are, by default.)
   class Config
     class <<self
       @@default_files = %w(~/.ssh/config /etc/ssh_config /etc/ssh/ssh_config)
+
+      # Returns an array of locations of OpenSSH configuration files
+      # to parse by default.
       def default_files
         @@default_files
       end
 
+      # Loads the configuration data for the given +host+ from all of the
+      # given +files+ (defaulting to the list of files returned by
+      # #default_files), translates the resulting hash into the options
+      # recognized by Net::SSH, and returns them.
       def for(host, files=default_files)
-        settings = load_all(files, host)
+        translate(files.inject({}) { |settings, file| load(file, host, settings) })
+      end
 
+      # Load the OpenSSH configuration settings in the given +file+ for the
+      # given +host+. If +settings+ is given, the options are merged into
+      # that hash, with existing values taking precedence over newly parsed
+      # ones. Returns a hash containing the OpenSSH options. (See
+      # #translate for how to convert the OpenSSH options into Net::SSH
+      # options.)
+      def load(file, host, settings={})
+        file = File.expand_path(file)
+        return settings unless File.readable?(file)
+
+        in_match = false
+        IO.foreach(file) do |line|
+          next if line =~ /^\s*(?:#.*)?$/
+
+          key, value = line.strip.split(/\s+/, 2)
+          key.downcase!
+
+          value = $1 if value =~ /^"(.*)"$/
+          value = case value.strip
+            when /^\d+$/ then value.to_i
+            when /^no$/i then false
+            when /^yes$/i then true
+            else value
+            end
+
+          if key == 'host'
+            in_match = (host =~ pattern2regex(value))
+          elsif in_match
+            if key == 'identityfile'
+              settings[key] ||= []
+              settings[key] << value
+            else
+              settings[key] ||= value
+            end
+          end
+        end
+
+        return settings
+      end
+
+      # Given a hash of OpenSSH configuration options, converts them into
+      # a hash of Net::SSH options. Unrecognized options are ignored. The
+      # +settings+ hash must have Strings for keys, all downcased, and
+      # the returned hash will have Symbols for keys.
+      def translate(settings)
         settings.inject({}) do |hash, (key, value)|
           case key
           when 'ciphers' then
@@ -67,46 +148,10 @@ module Net; module SSH
         end
       end
 
-      def load(file, host, settings={})
-        file = File.expand_path(file)
-        return settings unless File.readable?(file)
-
-        in_match = false
-        IO.foreach(file) do |line|
-          next if line =~ /^\s*(?:#.*)?$/
-
-          key, value = line.strip.split(/\s+/, 2)
-          key.downcase!
-
-          value = $1 if value =~ /^"(.*)"$/
-          value = case value.strip
-            when /^\d+$/ then value.to_i
-            when /^no$/i then false
-            when /^yes$/i then true
-            else value
-            end
-
-          if key == 'host'
-            in_match = (host =~ pattern2regex(value))
-          elsif in_match
-            if key == 'identityfile'
-              settings[key] ||= []
-              settings[key] << value
-            else
-              settings[key] ||= value
-            end
-          end
-        end
-
-        return settings
-      end
-
-      def load_all(files, host)
-        files.inject({}) { |settings, file| load(file, host, settings) }
-      end
-
       private
 
+        # Converts an ssh_config pattern into a regex for matching against
+        # host names.
         def pattern2regex(pattern)
           pattern = "^" + pattern.gsub(/\./, "\\.").
             gsub(/\?/, '.').
@@ -114,6 +159,7 @@ module Net; module SSH
           Regexp.new(pattern, true)
         end
 
+        # Converts the given size into an integer number of bytes.
         def interpret_size(size)
           case size
           when /k$/i then size.to_i * 1024
@@ -124,4 +170,5 @@ module Net; module SSH
         end
     end
   end
+
 end; end
