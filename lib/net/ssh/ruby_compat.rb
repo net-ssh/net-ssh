0e5486eb89fedd5cf0c6ede30eebdc2be35222a4
delano <delano@solutious.com>
2009-08-27 19:32:42 -0400

    Quick argument fix for Net::SSH::Compat.io_select

--- lib/net/ssh/ruby_compat.rb
+++ lib/net/ssh/ruby_compat.rb
@@ -21,6 +21,9 @@ module Net; module SSH
       def self.io_select(read_array, write_array=nil, error_array=nil, timeout=nil)
         IO.select(read_array, write_array, error_array, timeout)
       end
+      def self.io_select2(read_array, write_array=nil)
+        IO.select(read_array, write_array)
+      end
     else
       SELECT_MUTEX = Mutex.new
       def self.io_select(read_array, write_array=nil, error_array=nil, timeout=nil)
@@ -28,6 +31,11 @@ module Net; module SSH
           IO.select(read_array, write_array, error_array, timeout)
         end
       end
+      def self.io_select2(read_array, write_array=nil)
+         SELECT_MUTEX.synchronize do
+           IO.select(read_array, write_array)
+         end
+       end
     end
     
   end
