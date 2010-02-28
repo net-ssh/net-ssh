dc5af656f508852be73146baef9a795a54b7c7d1
Miklós Fazekas <mfazekas@szemafor.com>
2010-02-28 10:23:55 +0100

    Fixed first incarnation

--- endtoendtests/test_forward.rb
+++ endtoendtests/test_forward.rb
@@ -23,30 +23,39 @@ class TestForward < Test::Unit::TestCase
     8080
   end
   
-  def start_server_sending_lot_of_data
+  def start_server_sending_lot_of_data(exceptions=nil)
     server = TCPServer.open(0)
     Thread.start do
       loop do
         Thread.start(server.accept) do |client|
-          10000.times do |i| 
-            client.puts "item#{i}"
+          begin
+            10000.times do |i| 
+              client.puts "item#{i}"
+            end
+            client.close
+          rescue 
+            exceptions << $!
+            raise
           end
-          client.close
         end
       end
     end
     return server
   end
   
-  def start_server_closing_soon
+  def start_server_closing_soon(exceptions=nil)
     server = TCPServer.open(0)
     Thread.start do
       loop do
         Thread.start(server.accept) do |client|
-          client.recv(1024) 
-          client.setsockopt(Socket::SOL_SOCKET, Socket::SO_LINGER, [1, 0].pack("ii"))
-          client.close
-          #client.close
+          begin
+            client.recv(1024) 
+            client.setsockopt(Socket::SOL_SOCKET, Socket::SO_LINGER, [1, 0].pack("ii"))
+            client.close
+          rescue
+            exceptions <<  $!
+            raise
+          end
         end
       end
     end
@@ -54,8 +63,9 @@ class TestForward < Test::Unit::TestCase
   end
   
   def test_loop_should_not_abort_when_local_side_of_forward_is_closed
-    session = Net::SSH.start(*ssh_start_params)    
-    server = start_server_sending_lot_of_data
+    session = Net::SSH.start(*ssh_start_params) 
+    server_exc = Queue.new
+    server = start_server_sending_lot_of_data(server_exc)
     remote_port = server.addr[1]
     local_port = find_free_port 
     session.forward.local(local_port, localhost, remote_port)
@@ -71,11 +81,13 @@ class TestForward < Test::Unit::TestCase
       end
     end
     session.loop(0.1) { client_done.empty? }
+    assert_equal "Broken pipe", "#{server_exc.pop}"
   end
   
   def test_loop_should_not_abort_when_local_side_of_forward_is_reset
-    session = Net::SSH.start(*ssh_start_params)    
-    server = start_server_sending_lot_of_data
+    session = Net::SSH.start(*ssh_start_params)
+    server_exc = Queue.new    
+    server = start_server_sending_lot_of_data(server_exc)
     remote_port = server.addr[1]
     local_port = find_free_port+1
     session.forward.local(local_port, localhost, remote_port)
@@ -92,6 +104,7 @@ class TestForward < Test::Unit::TestCase
       end
     end
     session.loop(0.1) { client_done.empty? }
+    assert_equal "Broken pipe", "#{server_exc.pop}"
   end
   
   def test_loop_should_not_abort_when_server_side_of_forward_is_closed
