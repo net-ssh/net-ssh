9c648df2b0fb08f63fe43c1019ea337f71f787da
Miklos Fazekas <mfazekas@szemafor.com>
2015-09-13 11:43:47 +0200

    3.0.0.rc1 release prepare

--- lib/net/ssh/version.rb
+++ lib/net/ssh/version.rb
@@ -45,17 +45,17 @@ module Net; module SSH
     end
 
     # The major component of this version of the Net::SSH library
-    MAJOR = 2
+    MAJOR = 3
 
     # The minor component of this version of the Net::SSH library
-    MINOR = 10
+    MINOR = 0
 
     # The tiny component of this version of the Net::SSH library
-    TINY  = 1
+    TINY  = 0
 
     # The prerelease component of this version of the Net::SSH library 
     # nil allowed
-    PRE   = 'rc2'
+    PRE   = 'rc1'
 
     # The current version of the Net::SSH library as a Version instance
     CURRENT = new(*[MAJOR, MINOR, TINY, PRE].compact)
