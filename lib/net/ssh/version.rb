210d9103118f076f5537cb9875abc4ac14ae3d86
Miklos Fazekas <mfazekas@szemafor.com>
2015-09-19 13:14:25 +0200

    Regenerate gemspec for version 3.0.1.rc1

--- lib/net/ssh/version.rb
+++ lib/net/ssh/version.rb
@@ -51,11 +51,11 @@ module Net; module SSH
     MINOR = 0
 
     # The tiny component of this version of the Net::SSH library
-    TINY  = 0
+    TINY  = 1
 
     # The prerelease component of this version of the Net::SSH library 
     # nil allowed
-    PRE   = nil
+    PRE   = 'rc1'
 
     # The current version of the Net::SSH library as a Version instance
     CURRENT = new(*[MAJOR, MINOR, TINY, PRE].compact)
