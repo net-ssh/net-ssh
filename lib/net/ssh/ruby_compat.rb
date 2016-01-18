b7e64d1eede7e02f7e87169d6eff2f6df9bf0a24
Tiago Cardoso <tiago.cardoso@swisscom.com>
2016-01-18 14:02:12 +0100

    removing backports for ruby 1.8 and ruby 1.9 (both EOL, and unsupported since v3, according to documentation)

--- lib/net/ssh/ruby_compat.rb
+++ lib/net/ssh/ruby_compat.rb
@@ -1,16 +1,5 @@
 require 'thread'
 
-class String
-  if RUBY_VERSION < "1.9"
-    def getbyte(index)
-      self[index]
-    end
-    def setbyte(index, c)
-      self[index] = c
-    end
-  end
-end
-
 module Net; module SSH
   
   # This class contains miscellaneous patches and workarounds
