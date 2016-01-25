7118b07e542d12e69daa3e418ef5fc0e9b3657c4
Tiago Cardoso <tiago.cardoso@swisscom.com>
2016-01-25 17:43:19 +0100

    Revert "cleaned up the concept of the keepalive; it should only be sent if select timed out, that is, if any of the waits was unsuccessful"
    This reverts commit 0a5faa46e3c64aac6e54dea1c93832b30cbe2d52.

--- test/connection/test_session.rb
+++ test/connection/test_session.rb
@@ -417,12 +417,10 @@ module Connection
     def test_process_should_not_call_enqueue_message_unless_io_select_timed_out
       timeout = Net::SSH::Connection::Session::DEFAULT_IO_SELECT_TIMEOUT
       options = { :keepalive => true }
-      socket.stubs(:wait_readable).with(timeout).returns(socket)
+      socket.stubs(:wait_readable).with(timeout).returns(nil)
       socket.stubs(:recv).returns("x")
       transport.expects(:enqueue_message).never
-      sess = session(options)
-      sess.stubs(:process_reader).with(socket)
-      sess.process
+      session(options).process
     end
 
     def test_process_should_not_call_enqueue_message_unless_keepalive_interval_not_go_on
