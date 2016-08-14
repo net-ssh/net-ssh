7ebad22eb0b8d53bf8c51c0bc11b0bd646771027
Miklos Fazekas <mfazekas@szemafor.com>
2016-08-14 15:55:21 +0200

    Ignore Net::SSH::Disconnect when all channels closed

--- lib/net/ssh/connection/session.rb
+++ lib/net/ssh/connection/session.rb
@@ -120,7 +120,11 @@ module Net; module SSH; module Connection
     def close
       info { "closing remaining channels (#{channels.length} open)" }
       channels.each { |id, channel| channel.close }
-      loop(0.1) { channels.any? }
+      begin
+        loop(0.1) { channels.any? }
+      rescue Net::SSH::Disconnect
+        raise unless channels.empty?
+      end
       transport.close
     end
 
