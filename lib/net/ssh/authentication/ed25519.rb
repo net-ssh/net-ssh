1d2e1755b7b348be3afa71ff411ca2fd9181667e
Miklos Fazekas <mfazekas@szemafor.com>
2017-01-18 20:37:02 +0100

    Attempt to require rbnacl/libsodium, but don't care if not loaded, we care about rbnacl

--- lib/net/ssh/authentication/ed25519.rb
+++ lib/net/ssh/authentication/ed25519.rb
@@ -1,6 +1,11 @@
 gem 'rbnacl', '>= 3.2.0', '< 5.0'
 gem 'bcrypt_pbkdf', '~> 1.0' unless RUBY_PLATFORM == "java"
 
+begin
+  require 'rbnacl/libsodium'
+rescue LoadError # rubocop:disable Lint/HandleExceptions
+end
+
 require 'rbnacl'
 require 'rbnacl/signatures/ed25519/verify_key'
 require 'rbnacl/signatures/ed25519/signing_key'
