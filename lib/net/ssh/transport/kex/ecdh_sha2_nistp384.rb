10021820abcc570f1c2cad18915c21006b4e32e3
Florian Wininger <fw.centrale@gmail.com>
2019-08-23 16:21:50 +0200

    Remove already include modules
    Signed-off-by: Florian Wininger <fw.centrale@gmail.com>

--- lib/net/ssh/transport/kex/ecdh_sha2_nistp384.rb
+++ lib/net/ssh/transport/kex/ecdh_sha2_nistp384.rb
@@ -2,7 +2,6 @@ module Net
   module SSH 
     module Transport 
       module Kex
-
         # A key-exchange service implementing the "ecdh-sha2-nistp256"
         # key-exchange algorithm. (defined in RFC 5656)
         class EcdhSHA2NistP384 < EcdhSHA2NistP256
