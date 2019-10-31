28e44a0382a2806fdc0f9f078037d403bc62a154
Anders Carling <lowe@lowe.nu>
2019-10-31 01:15:21 +0100

    Test explicit cert configuration

--- test/integration/test_cert_user_auth.rb
+++ test/integration/test_cert_user_auth.rb
@@ -9,17 +9,28 @@ unless ENV['NET_SSH_NO_ED25519']
 
   class TestCertUserAuth < NetSSHTest
     include IntegrationTestHelpers
-  
-    def test_ed25519_with_cert
+
+    def test_ed25519_with_implicit_cert
       Dir.mktmpdir do |dir|
         sh "rm -rf #{dir}/id_rsa_ed25519 #{dir}/id_rsa_ed25519.pub"
         sh "ssh-keygen -q -f #{dir}/id_rsa_ed25519 -t ed25519 -N ''"
         sign_user_key('net_ssh_1',"#{dir}/id_rsa_ed25519.pub")
-  
-        # sshopts = '-vvvv -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
-        # sh "ssh -i #{dir}/id_rsa_ed25519 #{sshopts} net_ssh_1@localhost echo 'hello'"
-  
+
         ret = Net::SSH.start("localhost", "net_ssh_1", keys: "#{dir}/id_rsa_ed25519") do |ssh|
+          ssh.exec! 'echo "hello from:$USER"'
+        end
+        assert_equal "hello from:net_ssh_1\n", ret
+      end
+    end
+
+    def test_ed25519_with_explicit_cert
+      Dir.mktmpdir do |dir|
+        sh "rm -rf #{dir}/id_rsa_ed25519 #{dir}/id_rsa_ed25519.pub"
+        sh "ssh-keygen -q -f #{dir}/id_rsa_ed25519 -t ed25519 -N ''"
+        sign_user_key('net_ssh_1',"#{dir}/id_rsa_ed25519.pub")
+        sh "mv #{dir}/id_rsa_ed25519-cert.pub #{dir}/cert"
+
+        ret = Net::SSH.start("localhost", "net_ssh_1", keys: "#{dir}/id_rsa_ed25519", keycerts: "#{dir}/cert") do |ssh|
           ssh.exec! 'echo "hello from:$USER"'
         end
         assert_equal "hello from:net_ssh_1\n", ret
@@ -36,6 +47,24 @@ unless ENV['NET_SSH_NO_ED25519']
           sh "rm -rf #{dir}/id_rsa_ed25519 #{dir}/id_rsa_ed25519.pub #{dir}/id_rsa_ed25519-cert.pub"
 
           ret = Net::SSH.start("localhost", "net_ssh_1") do |ssh|
+            ssh.exec! 'echo "hello from:$USER"'
+          end
+          assert_equal "hello from:net_ssh_1\n", ret
+        end
+      end
+    end
+
+    def test_ed25519_with_key_in_agent_and_explicit_cert
+      Dir.mktmpdir do |dir|
+        with_agent do
+          sh "rm -rf #{dir}/id_rsa_ed25519 #{dir}/id_rsa_ed25519.pub"
+          sh "ssh-keygen -q -f #{dir}/id_rsa_ed25519 -t ed25519 -N ''"
+          # add key before signing cert
+          ssh_add("#{dir}/id_rsa_ed25519", "pwd")
+          sign_user_key('net_ssh_1',"#{dir}/id_rsa_ed25519.pub")
+          sh "rm -rf #{dir}/id_rsa_ed25519 #{dir}/id_rsa_ed25519.pub"
+
+          ret = Net::SSH.start("localhost", "net_ssh_1", keycerts: "#{dir}/id_rsa_ed25519-cert.pub") do |ssh|
             ssh.exec! 'echo "hello from:$USER"'
           end
           assert_equal "hello from:net_ssh_1\n", ret
