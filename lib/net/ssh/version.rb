139126b08ded6fe2451c3791a1c041f378920a05
Miklos Fazekas <mfazekas@szemafor.com>
2020-04-25 17:21:22 +0200

    6.1.0.rc1

--- lib/net/ssh/version.rb
+++ lib/net/ssh/version.rb
@@ -49,14 +49,14 @@ module Net
       MAJOR = 6
 
       # The minor component of this version of the Net::SSH library
-      MINOR = 0
+      MINOR = 1
 
       # The tiny component of this version of the Net::SSH library
-      TINY  = 1
+      TINY  = 0
 
       # The prerelease component of this version of the Net::SSH library
       # nil allowed
-      PRE   = nil
+      PRE   = "rc1"
 
       # The current version of the Net::SSH library as a Version instance
       CURRENT = new(*[MAJOR, MINOR, TINY, PRE].compact)
