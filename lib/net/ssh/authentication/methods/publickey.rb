00ca3c624d20a0c9592fe2b68ac333a0e891af36
Zack <zack.newsham@gmail.com>
2023-01-06 14:56:51 -0500

    Update publickey.rb

--- lib/net/ssh/authentication/methods/publickey.rb
+++ lib/net/ssh/authentication/methods/publickey.rb
@@ -97,9 +97,11 @@ module Net
               pubkey_algorithms.each do |pk_alg|
                 case pk_alg
                 when "rsa-sha2-512", "rsa-sha2-256", "ssh-rsa"
-                  if authenticate_with_alg(identity, next_service, username, pk_alg, pk_alg)
-                    # success
-                    return true
+                  if session.transport.algorithms.supported_signature_algs.includes? ok_alg
+                    if authenticate_with_alg(identity, next_service, username, type, pk_alg)
+                      # success
+                      return true
+                    end
                   end
                 end
               end
