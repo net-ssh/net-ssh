d02978214d01872b1f497e1020f9e039b9742c74
Mike Atlas <matlas@everquote.com>
2024-01-17 00:04:48 -0500

    Fix syntax support for Ruby 2.4

--- lib/net/ssh/config.rb
+++ lib/net/ssh/config.rb
@@ -187,10 +187,10 @@ module Net
           begin
             default_files.keep_if do |path|
               File.expand_path(path)
-              true
+              return true
             end
           rescue ArgumentError
-            false
+            return false
           end
         end
 
