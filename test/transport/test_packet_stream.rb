4ffb3843f50b5803bcaee5f156d85484fdc5f1d2
Josh Goodall <inopinatus@inopinatus.org>
2024-05-11 18:07:38 +1000

    Tell any ProxyCommand process to hang up.
    If there's a pid in the IO, send it a SIGHUP on cleanup.
    
    Solves a problem when using EC2 Instance Connect, which doesn't
    handle shutdown across the tunnel the way a regular socket might,
    leading to zombie sessions on the server and net-ssh hanging on
    close.  May resolve other similar issues with unconventional proxy
    configurations.
    
    Found by comparison with OpenSSH, which also sends a HUP, so this
    is behavioural parity.

--- test/transport/test_packet_stream.rb
+++ test/transport/test_packet_stream.rb
@@ -69,6 +69,7 @@ module Transport
     def test_cleanup_should_delegate_cleanup_to_client_and_server_states
       stream.client.expects(:cleanup)
       stream.server.expects(:cleanup)
+      stream.expects(:pid)
       stream.cleanup
     end
 
@@ -1102,6 +1103,7 @@ module Transport
             assert packet[:always_display]
             assert_equal "debugging", packet[:message]
             assert_equal "", packet[:language]
+            stream.stubs(:pid).returns(nil)
             stream.cleanup
           end
 
@@ -1120,6 +1122,7 @@ module Transport
             stream.client.set cipher: cipher, hmac: hmac, compression: compress
             stream.enqueue_packet(ssh_packet)
             assert_equal PACKETS[cipher_name][hmac_name][compress], stream.write_buffer
+            stream.stubs(:pid).returns(nil)
             stream.cleanup
           end
         end
